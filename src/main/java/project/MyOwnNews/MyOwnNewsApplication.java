5f2a3044393e430e5c25f80a1bfc9f59e8b3e0b6
chabee <jhcha990917@gmail.com>
2025-02-18 18:31:53 +0900

    Initial commit

---
+++ src/main/java/project/MyOwnNews/MyOwnNewsApplication.java
@@ -0,0 +1,13 @@
+package project.MyOwnNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/MyOwnNews/MyOwnNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.MyOwnNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
